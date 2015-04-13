04e3d37da6d8b4a2bcff068ba0aa41f934bd73b2
tiger.guo <43375138@qq.com>
2015-04-13 16:13:51 +0800

    tt

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -22,6 +22,7 @@ class HelloController extends Controller
     /**
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
+     * test commit
      */
     public function actionIndex($message = 'hello world')
     {
